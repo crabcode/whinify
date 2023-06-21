4c42eb27cf675879ea61c0bfa75ce978312f59f1
Crabman <crabman@hypnoticowl.com>
2023-06-21 13:10:21 +0200

    Fix for mobile browsers

--- whinify.js
+++ whinify.js
@@ -3,23 +3,17 @@ targets = [
 	'textarea'
 ];
 
-function whinify(element, event)
+function whinify(element)
 {
-	const key = event.data;
-	
-	if (/^[a-zA-Z]$/.test(key))
-	{
-		event.preventDefault();
+	const value = element.value;
+	const start = element.selectionStart - 1;
+	const end = element.selectionEnd;
+	const key = value.substring(start, end);
 		
-		const value = element.value;
-		const start = element.selectionStart - 1;
-		const end = element.selectionEnd;
+	let modKey = start % 2 == 1 ? key.toUpperCase() : key.toLowerCase();
 		
-		let modKey = start % 2 == 1 ? key.toUpperCase() : key.toLowerCase();
-		
-		element.value = value.substring(0, start) + modKey + value.substring(end);
-		element.setSelectionRange(start + 1, start + 1);
-	}
+	element.value = value.substring(0, start) + modKey + value.substring(end);
+	element.setSelectionRange(start + 1, start + 1);
 }
 
-window.onload = () => document.querySelectorAll(targets.join(",")).forEach(element => element.addEventListener("input", event => whinify(element, event)));
+window.onload = () => document.querySelectorAll(targets.join(",")).forEach(element => element.addEventListener("input", e => whinify(element)));
